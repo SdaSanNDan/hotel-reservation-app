d7371cb613705f09539b9322f3a15ae29d2cc02c
Hector <shaba@gmail.com>
2019-01-03 19:57:58 +0200

    addded fragments and cleaned up some of the HTML

---
+++ src/main/java/com/sda/bookingproject/reservation/app/controller/NewsletterController.java
@@ -0,0 +1,37 @@
+package com.sda.bookingproject.reservation.app.controller;
+
+
+import com.sda.bookingproject.reservation.app.model.NewsletterModel;
+import com.sda.bookingproject.reservation.app.service.NewsletterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class NewsletterController {
+
+    @Autowired
+    private NewsletterService newsletterService;
+
+    @GetMapping("/newsletter")
+    public ModelAndView indexPage(){
+
+        return new ModelAndView("index")
+                .addObject("newsletter", new NewsletterModel())
+                .addObject("user", "SANDER");
+
+    }
+
+    @PostMapping("/newsletter")
+    public ModelAndView insertNewsLetter(@ModelAttribute("email") final String email){
+
+        newsletterService.insertNewsletter(email);
+        return new ModelAndView("result").addObject("result", email);
+    }
+
+}
