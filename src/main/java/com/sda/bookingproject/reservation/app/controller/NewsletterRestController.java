c2810f1c81e5f84fb6be511f36062423a3b1e316
Hector <shaba@gmail.com>
2019-01-04 18:59:05 +0200

    added API connection to newsletter template

--- src/main/java/com/sda/bookingproject/reservation/app/controller/NewsletterRestController.java
+++ src/main/java/com/sda/bookingproject/reservation/app/controller/NewsletterRestController.java
@@ -1,8 +1,6 @@
 package com.sda.bookingproject.reservation.app.controller;
 
-import com.sda.bookingproject.reservation.app.domain.NewsletterEntity;
 import com.sda.bookingproject.reservation.app.model.NewsletterModel;
-import com.sda.bookingproject.reservation.app.repository.NewsLetterRepository;
 import com.sda.bookingproject.reservation.app.service.NewsletterService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 import java.util.List;
-import java.util.Optional;
 
 @Data
 @RestController
@@ -50,7 +46,7 @@ public class NewsletterRestController {
     }
 
     @PutMapping("/newsletter")
-    public HttpStatus updateNewsletterById(@Valid @RequestBody final NewsletterModel newsletterModel){
+    public HttpStatus updateNewsletterById(@Valid @RequestBody final NewsletterModel newsletterModel) {
 
         newsletterService.updateNewsletter(newsletterModel);
 
